6653827441e78b208ef73d02de79259a92359658
syed mahamud <mahmud@syftet.com>
2022-01-24 01:02:41 +0600

    landing page design

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
    # devise_for :users
-   devise_for :users, :controllers => { :sessions => "users/sessions" }
-  resources :users
-  # devise_for :users, controllers: {
-  #   sessions: 'users/sessions',
-  #   registrations: 'users/registrations',
-  #   passwords: 'users/passwords',
-  #   confirmations: 'users/confirmations'
-  # }
-  get 'dashboard', to: 'home#index', as: :dashboard
+   #devise_for :users, :controllers => { :sessions => "users/sessions" }
+   devise_for :users, controllers: {
+     sessions: 'users/sessions',
+     registrations: 'users/registrations',
+     passwords: 'users/passwords',
+     confirmations: 'users/confirmations'
+  }
+   resources :users
+   get 'dashboard', to: 'home#index', as: :dashboard
   get 'profile', to: 'users#profile', as: :profile
   get 'edit_profile', to: 'users#edit_profile', as: :edit_profile
    patch 'update_profile', to: 'users#update_profile', as: :update_profile
