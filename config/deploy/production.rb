699aa65d636ea639a18285cbbb5dc2527d2d8a4a
Syed Mahamud <syedmahamud25@gmail.com>
2025-05-14 19:54:22 +0600

    cucse config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,7 +59,8 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-set :deploy_to, "/home/deployer/apps/bssas"
+# set :deploy_to, "/home/deployer/apps/bssas"
+set :deploy_to, "/home/deployer/apps/cucse"
 set :rails_env, "production"
 
 set :branch, 'production'
