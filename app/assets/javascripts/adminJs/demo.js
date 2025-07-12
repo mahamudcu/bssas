3c684fbdd5721f00c3462485412e86b4e17db893
Syed Mahamud <syedmahamud25@gmail.com>
2025-07-12 13:08:48 +0600

    dashboard cleaning

--- app/assets/javascripts/adminJs/demo.js
+++ app/assets/javascripts/adminJs/demo.js
@@ -60,7 +60,7 @@ var statisticsChart = new Chart(ctx, {
 	data: {
 		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
 		datasets: [ {
-			label: "Subscribers",
+			label: "Registrations",
 			borderColor: '#f3545d',
 			pointBackgroundColor: 'rgba(243, 84, 93, 0.6)',
 			pointRadius: 0,
@@ -70,7 +70,7 @@ var statisticsChart = new Chart(ctx, {
 			borderWidth: 2,
 			data: [154, 184, 175, 203, 210, 231, 240, 278, 252, 312, 320, 374]
 		}, {
-			label: "New Visitors",
+			label: "Alumni",
 			borderColor: '#fdaf4b',
 			pointBackgroundColor: 'rgba(253, 175, 75, 0.6)',
 			pointRadius: 0,
@@ -80,7 +80,7 @@ var statisticsChart = new Chart(ctx, {
 			borderWidth: 2,
 			data: [256, 230, 245, 287, 240, 250, 230, 295, 331, 431, 456, 521]
 		}, {
-			label: "Active Users",
+			label: "Members",
 			borderColor: '#177dff',
 			pointBackgroundColor: 'rgba(23, 125, 255, 0.6)',
 			pointRadius: 0,
